18d9029ab985c8d7ea4811217f19e12fa3567302
ParkerSwinton <54301961+ParkerSwinton@users.noreply.github.com>
2019-12-20 14:04:27 -0800

    Implemented flexbox for cal names + cleaned up mainClock

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 function mainClock() {
 	var clock = document.getElementById("main-clock");
-	var y = new Date().toLocaleTimeString();
-	clock.innerHTML = y;
-	setTimeout(mainClock, 1000);
+	clock.innerHTML = new Date().toLocaleTimeString();
 }
