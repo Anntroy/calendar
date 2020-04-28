62feaeca91f3ab8e2c166491f17a2bbeb0d8a040
ParkerSwinton <54301961+ParkerSwinton@users.noreply.github.com>
2020-04-27 22:43:17 -0700

    Clean up / Comment out code

--- main.js
+++ main.js
@@ -2,38 +2,38 @@
 window.onload = function(){
 	
 	function mainClock() {
-		var clock = document.getElementById("main-clock");
+		let clock = document.getElementById("main-clock");
 		clock.innerHTML = new Date().toLocaleTimeString();
 	}
 	function updateCalendar(){
 		
 		//Finding the first day of week to start at
 		//and the number of days in the month
-		var firstDay = dateFns.startOfMonth(currMonth);
-		var lastDay = dateFns.endOfMonth(currMonth);
+		let firstDay = dateFns.startOfMonth(currMonth);
+		let lastDay = dateFns.endOfMonth(currMonth);
 		firstDay = dateFns.getDay(firstDay);
 		lastDay = dateFns.getDate(lastDay);
 		
-		var date = 1;
+		let date = 1;
 
 		//This is a loop for the weeks.
-		for(var j = 0; j < 6; j++){
+		for(let j = 0; j < 6; j++){
 			
-			var week = document.getElementById("calendar-root").children[j];
+			let week = document.getElementById("calendar-root").children[j];
 			
 			//This is a loop for the days.
-			for(var i = 0; i<7;i++){
+			for(let i = 0; i < 7; i++){
 
-				var day = week.children[i];
+				let day = week.children[i];
 				//Deciding to erase the block or add date.
 				if (i<firstDay || date > lastDay){
 					day.style.opacity = 0;
 					day.innerHTML = "";
-					day.removeEventListener("click",imageAppear);
+					day.removeEventListener("click", null);
 				} else {
 					day.innerHTML = date++;
 					day.style.opacity = 1;
-					day.addEventListener("click",imageAppear);
+					day.addEventListener("click", null);
 				}
 
 			}	
@@ -59,7 +59,7 @@ window.onload = function(){
 		updateCalendar();
 	}
 	
-	async function newImage(){
+/*	async function newImage(){
 		let response = await fetch('https://dog.ceo/api/breeds/image/random');
 		let data = await response.json();
 		let url = data.message;
@@ -80,16 +80,17 @@ window.onload = function(){
 		document.getElementById("image-container").style.display = "none";
 		newImage();
 	}
-	
+*/
 
-	var currMonth = new Date();
-	var yearTrack = dateFns.getYear(currMonth);
-	var monthTrack = dateFns.getMonth(currMonth);
+	let currMonth = new Date();
+	let yearTrack = dateFns.getYear(currMonth);
+	let monthTrack = dateFns.getMonth(currMonth);
 	updateCalendar();
 	setInterval(mainClock,500);
-	document.getElementById("left").addEventListener("click",function(){updateMonth(-1)});
-	document.getElementById("right").addEventListener("click",function(){updateMonth(1)});
-	document.getElementById("close").addEventListener("click", exitOut);
-	newImage();
+	document.getElementById("left").addEventListener("click", () => {updateMonth(-1)});
+	document.getElementById("right").addEventListener("click", () => {updateMonth(1)});
+	
+	//document.getElementById("close").addEventListener("click", exitOut);
+	//newImage();
 	
 }
